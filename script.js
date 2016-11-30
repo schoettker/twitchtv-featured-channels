fe7577e56cfa404b032fe68f1b693953f6517f56
schoettker <lennartschoettker@hotmail.com>
2016-11-30 21:38:08 +0100

    send XHR and retrieve featured TwitchTV Channels; appends their title and description to a link and appends this to a list item

---
+++ script.js
@@ -0,0 +1,41 @@
+function sendRequest() {
+  var request = new XMLHttpRequest(), key = '4tkqp56p8riaa8nvg50n1b5xjqzt1mx', url = 'https://api.twitch.tv/kraken/streams/featured';
+  request.open('GET', url, true);
+  request.setRequestHeader("Client-ID", key);
+  request.onload = function() {
+    if (this.status >= 200 && this.status < 400) {
+      // Success!
+      var data = JSON.parse(this.response).featured;
+      console.log(data);
+      createList(data);
+    } else {
+      // We reached our target server, but it returned an error
+      console.log("error");
+    }
+  };
+
+  request.onerror = function() {
+    // There was a connection error of some sort
+      console.log("error");
+  };
+
+  request.send();
+}
+sendRequest();
+
+function createList(arr) {
+  var outputList = document.getElementById('output');
+  for (var i = 0, l = arr.length; i < l; i++) {
+    var current = arr[i];
+    var listitem = document.createElement('li'), title = document.createElement('h5'), div = document.createElement('div'), link = document.createElement('a');
+    title.appendChild(document.createTextNode(current.title));
+    div.classList.add('channel-description');
+    div.innerHTML = current.text;
+    link.appendChild(title);
+    link.appendChild(div);
+    link.setAttribute('href', current.stream.channel.url);
+    link.setAttribute('target', 'blank');
+    listitem.appendChild(link);
+    outputList.appendChild(listitem);
+  }
+}
