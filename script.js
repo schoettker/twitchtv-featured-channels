cf16d6f8b50fc71526266a4eddd2d8c6ae6c8382
schoettker <lennartschoettker@hotmail.com>
2016-12-01 21:25:35 +0100

    add function to send XHR for specific streams (stored in array) and display their info or an error message if they are not found(do not exist); still need to handle channels that exist but are offline

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-function sendRequest(url, handleData) {
+function sendRequest(url, handleData, type, channelName) {
   var request = new XMLHttpRequest(), key = '4tkqp56p8riaa8nvg50n1b5xjqzt1mx';
   request.open('GET', url, true);
   request.setRequestHeader("Client-ID", key);
@@ -7,10 +7,19 @@ function sendRequest(url, handleData) {
       // Success!
       var data = JSON.parse(this.response);
       console.log(data);
-      handleData(data);
+      handleData(data, type);
     } else {
       // We reached our target server, but it returned an error
       console.log("error");
+      var outputList = document.getElementById('output');
+      var listitem = document.createElement('li'), title = document.createElement('h5'), div = document.createElement('div');
+      title.appendChild(document.createTextNode('Channel ' + channelName + ' not found ;('));
+      div.appendChild(document.createTextNode('The channel is offline or does not exist'));
+      div.classList.add('channel-description');
+      // div.innerHTML = obj.text;
+      listitem.appendChild(title);
+      listitem.appendChild(div);
+      outputList.appendChild(listitem);
     }
   };
 
@@ -22,29 +31,53 @@ function sendRequest(url, handleData) {
   request.send();
 }
 
-function createList(arr) {
-  var outputList = document.getElementById('output'), arr = arr.featured;
-  for (var i = 0, l = arr.length; i < l; i++) {
-    var current = arr[i];
-    // required DOM Elements
+function createListItem(obj, type) {
+  var outputList = document.getElementById('output');
+  function featured(obj) {
     var listitem = document.createElement('li'), title = document.createElement('h5'), div = document.createElement('div'), link = document.createElement('a');
-
-    title.appendChild(document.createTextNode(current.title));
+    title.appendChild(document.createTextNode(obj.title));
     div.classList.add('channel-description');
-    div.innerHTML = current.text;
+    div.innerHTML = obj.text;
     link.appendChild(title);
     link.appendChild(div);
-    link.setAttribute('href', current.stream.channel.url);
+    link.setAttribute('href', obj.stream.channel.url);
     link.setAttribute('target', 'blank');
     listitem.appendChild(link);
     outputList.appendChild(listitem);
   }
+  function specific(obj) {
+    var listitem = document.createElement('li'), title = document.createElement('h5'), div = document.createElement('div'), link = document.createElement('a');
+    title.appendChild(document.createTextNode(obj.stream.channel.name));
+    div.appendChild(document.createTextNode(obj.stream.channel.status));
+    div.classList.add('channel-description');
+    // div.innerHTML = obj.text;
+    link.appendChild(title);
+    link.appendChild(div);
+    link.setAttribute('href', obj.stream.channel.url);
+    link.setAttribute('target', 'blank');
+    listitem.appendChild(link);
+    outputList.appendChild(listitem);
+
+  }
+
+  if (type == 'featured') {
+    for (var i = 0, l = obj.featured.length; i < l; i++) {
+      var v = obj.featured[i];
+      featured(v);
+    }
+  } else if (type == 'specific') {
+    specific(obj);
+  }
+
 }
 function featuredChannels() {
-  sendRequest('https://api.twitch.tv/kraken/streams/featured', createList);
+  sendRequest('https://api.twitch.tv/kraken/streams/featured', createListItem, 'featured');
 }
-featuredChannels();
-// function specificChannels() {
-//   sendRequest('https://api.twitch.tv/kraken/streams/freecodecamp');
-// }
-// specificChannels();
+// featuredChannels();
+function specificChannels(streamNames) {
+  streamNames.forEach(function(stream) {
+    sendRequest('https://api.twitch.tv/kraken/streams/' + stream, createListItem, "specific", stream);
+  });
+}
+var streamNames = ['imaqtpie', 'DominGo', 'overpow', 'dfs84382dfsfs', 'freecodecamp'];
+specificChannels(streamNames);
